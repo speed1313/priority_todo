e8b8baf5849b389f4a592587efac747f5995f9cc
speed1313 <speedtry13@icloud.com>
2022-05-09 18:45:43 +0900

    impl add feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use clap::Parser;
+use std::io;
+use std::fs;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long)]
+    method: String,
+
+    /// Number of times to greet
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match &*args.method{
+        "add" => add(),
+        "complete" => complete(),
+        "show" => show(),
+        _ => panic!()
+    };
+}
+
+
+fn add()->io::Result<()>{
+    let mut buffer = String::new();
+    let mut stdin = io::stdin();
+    stdin.read_line(&mut buffer)?;
+    fs::write("./todolist.txt",&buffer)?;
+    println!("{}",buffer);
+    Ok(())
+
+}
+
+fn complete()->io::Result<()>{
+    Ok(())
+}
+
+fn show()->io::Result<()>{
+    Ok(())
 }
